b626cac28b62b4ae00850766371efdca9350aa30
Maarten de Jong <me@arteeh.com>
2024-07-26 23:52:37 +0200

    geniso script still broken

--- generate-isos.sh
+++ generate-isos.sh
@@ -1,38 +1,39 @@
 docker run \
+    --name geniso-main \
 	--rm \
 	--privileged \
-	--volume ~silvercore-iso/main:/build-container-installer/build  \
+	--volume ./iso/main:/build-container-installer/build  \
 	ghcr.io/jasonn3/build-container-installer:latest \
 	VERSION=40 \
 	IMAGE_REPO=ghcr.io/arteeh \
 	IMAGE_NAME=silvercore \
 	IMAGE_TAG=40 \
 	VARIANT=Silverblue \
-	ISO_NAME=Silvercore \
-	WEB_UI=true
-
+	ISO_NAME=Silvercore.iso \
+	WEB_UI=true & \
 docker run \
+    --name geniso-nvidia \
 	--rm \
 	--privileged \
-	--volume ~silvercore-iso/nvidia:/build-container-installer/build  \
+	--volume ./iso/nvidia:/build-container-installer/build  \
 	ghcr.io/jasonn3/build-container-installer:latest \
 	VERSION=40 \
 	IMAGE_REPO=ghcr.io/arteeh \
 	IMAGE_NAME=silvercore-nvidia \
 	IMAGE_TAG=40 \
 	VARIANT=Silverblue \
-	ISO_NAME=Silvercore-NVIDIA \
-	WEB_UI=true
-
+	ISO_NAME=Silvercore-NVIDIA.iso \
+	WEB_UI=true & \
 docker run \
+    --name geniso-server \
 	--rm \
 	--privileged \
-	--volume ~silvercore-iso/server:/build-container-installer/build  \
+	--volume ./iso/server:/build-container-installer/build  \
 	ghcr.io/jasonn3/build-container-installer:latest \
 	VERSION=40 \
 	IMAGE_REPO=ghcr.io/arteeh \
 	IMAGE_NAME=silvercore-server \
 	IMAGE_TAG=40 \
 	VARIANT=Server \
-	ISO_NAME=Silvercore-Server \
+	ISO_NAME=Silvercore-Server.iso \
 	WEB_UI=true
