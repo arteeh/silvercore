39da64ff483a128df05769a57a259eca367350a2
Maarten de Jong <35239587+arteeh@users.noreply.github.com>
2024-08-20 14:29:08 +0200

    Update generate-isos.sh

--- generate-isos.sh
+++ generate-isos.sh
@@ -7,8 +7,10 @@ docker run \
 	VERSION=40 \
 	IMAGE_REPO=ghcr.io/arteeh \
 	IMAGE_NAME=silvercore \
-	IMAGE_TAG=40 \
+	IMAGE_TAG=latest \
 	VARIANT=Silverblue \
+ 	SECURE_BOOT_KEY_URL=https://raw.githubusercontent.com/arteeh/silvercore/main/cosign.pub \
+	ENROLLMENT_PASSWORD=universalblue \
 	ISO_NAME=build/Silvercore.iso \
 	WEB_UI=true & \
 docker run \
@@ -20,8 +22,10 @@ docker run \
 	VERSION=40 \
 	IMAGE_REPO=ghcr.io/arteeh \
 	IMAGE_NAME=silvercore-nvidia \
-	IMAGE_TAG=40 \
+	IMAGE_TAG=latest \
 	VARIANT=Silverblue \
+ 	SECURE_BOOT_KEY_URL=https://raw.githubusercontent.com/arteeh/silvercore/main/cosign.pub \
+	ENROLLMENT_PASSWORD=universalblue \
 	ISO_NAME=build/Silvercore-NVIDIA.iso \
 	WEB_UI=true & \
 docker run \
@@ -33,7 +37,24 @@ docker run \
 	VERSION=40 \
 	IMAGE_REPO=ghcr.io/arteeh \
 	IMAGE_NAME=silvercore-server \
-	IMAGE_TAG=40 \
+	IMAGE_TAG=latest \
 	VARIANT=Server \
+ 	SECURE_BOOT_KEY_URL=https://raw.githubusercontent.com/arteeh/silvercore/main/cosign.pub \
+	ENROLLMENT_PASSWORD=universalblue \
 	ISO_NAME=build/Silvercore-Server.iso \
+	WEB_UI=true & \
+ docker run \
+    --name geniso-cosmic \
+	--rm \
+	--privileged \
+	--volume ./iso/server:/build-container-installer/build  \
+	ghcr.io/jasonn3/build-container-installer:latest \
+	VERSION=40 \
+	IMAGE_REPO=ghcr.io/arteeh \
+	IMAGE_NAME=silvercore-cosmic \
+	IMAGE_TAG=latest \
+	VARIANT=Kinoite \
+ 	SECURE_BOOT_KEY_URL=https://raw.githubusercontent.com/arteeh/silvercore/main/cosign.pub \
+	ENROLLMENT_PASSWORD=universalblue \
+	ISO_NAME=build/Silvercore-Cosmic.iso \
 	WEB_UI=true
